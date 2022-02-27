eec65fd8cde2a9150bd24f726565ba2d5c11c9cd
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2022-02-27 12:23:12 -0500

    first commit

---
+++ src/test/java/com/bank/debitCard/DebitCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.debitCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
